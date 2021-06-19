6c6e5ab7de31a2a10f9ade253a109b05cfa9f394
Human <ainur.hak2004@gmail.com>
2021-06-19 14:40:07 +0300

    Working TG bot

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,42 +2,72 @@
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
+using Telegram.Bot;
 
 namespace Api
 {
     class Program
     {
+        [Obsolete]
         static void Main(string[] args)
         {
-            var Word = "Example";
+            TelegramBotClient bot = new TelegramBotClient("1806549539:AAGFkzLCXqDLNTNSwXEHblm-kDffkNDJYao");
+
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                Api(arg.Message.Text, arg.Message.Chat.Id, bot);
+            };
+
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+
+        public static void Api(string Word, long botID, TelegramBotClient bot)
+        {
             var url = $"https://wordsapiv1.p.rapidapi.com/words/{Word}";
 
             var request = WebRequest.Create(url);
             request.Headers.Add("x-rapidapi-key", "7dc9c2102cmsh590a9a30e7366a2p186d71jsn428e1160ef63");
             request.Headers.Add("x-rapidapi-host", "wordsapiv1.p.rapidapi.com");
 
-            var response = request.GetResponse();
-            var httpStatusCode = (response as HttpWebResponse).StatusCode;
-
-            if (httpStatusCode != HttpStatusCode.OK)
+            try
             {
-                Console.WriteLine(httpStatusCode);
-                return;
-            }
+                var response = request.GetResponse();
 
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
-            {
-                string result = streamReader.ReadToEnd();
-                var word = JsonConvert.DeserializeObject<Root>(result);
-                foreach (var el in word.results)
+
+                var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+                if (httpStatusCode != HttpStatusCode.OK)
                 {
-                    if (el.examples != null)
+                    Console.WriteLine(httpStatusCode);
+                    return;
+                }
+
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string result = streamReader.ReadToEnd();
+                    var word = JsonConvert.DeserializeObject<Root>(result);
+                    foreach (var el in word.results)
                     {
-                        Console.WriteLine(el.examples[0]);
+                        if (el.examples != null)
+                        {
+                            Console.WriteLine(el.examples[0]);
+                            bot.SendTextMessageAsync(botID, el.examples[0]);
+                        }
                     }
                 }
             }
-            Console.ReadKey();
+            catch (WebException e)
+            {
+                Console.WriteLine("\r\nWebException Raised. The following error occured : {0}", e.Status);
+                bot.SendTextMessageAsync(botID, "Invalid word, try again!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exeption");
+            }
         }
     }
 }
